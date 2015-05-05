b84851bd7d4058b94c7156e3ef715bc69333a8d4
colemand77 <dmacaricoleman@gmail.com>
2015-05-05 18:09:11 -0500

    Changed the data cleansing process to account for St Martin parish being non-continguous. All St. Martin rig counts will go into St. Also filters on Land only counts now Also changed the getCountData() function to process on exact matches

--- rigData.R
+++ rigData.R
@@ -9,19 +9,16 @@ adj<- as.data.table(raw)
 adj[, `:=`(PublishDate = as.Date(PublishDate, format = "%m/%d/%Y"),
            State.Province = tolower(State.Province),
            County = tolower(County))]
-adj[County == "st. martin", County := "st martin"]
+adj[County == "st. martin", County := "st martin:north"]
 adj[, `:=`(region = State.Province,
           subregion = County, 
           adjName = paste(adj$State.Province, adj$County, sep = ","))]
 
-rig_County_names <- unique(adj[Country == "UNITED STATES" & State.Province != "Alaska" & State.Province != "Hawaii"
+rig_County_names <- unique(adj[Country == "UNITED STATES" & State.Province != "alaska" & Locatin == "Land" & State.Province != "hawaii"
                                ,adjName])
 
 rigCountDates <- 1:length(unique(adj[,PublishDate]))
 names(rigCountDates) <-  unique(adj[,PublishDate])
-
-str(rig_County_names)
-rig_County_names
 
 #note I used approximate matching here. THis could be a problem going forward.
 RigCountyMap <- map("county", region  = rig_County_names,
@@ -29,9 +26,12 @@ RigCountyMap <- map("county", region  = rig_County_names,
 
 getCountData <- function(date){
   temp <- adj[PublishDate == as.Date(date),]
-  temp_County_names <- unique(temp[Country == "UNITED STATES" & State.Province != "Alaska" & State.Province != "Hawaii"
-                                         ,adjName])
-  tempCountyMap <- map("county", region  = temp_County_names,
-                      plot=FALSE, exact = FALSE, fill = TRUE)
+  temp_County_names <- unique(temp[Country == "UNITED STATES" & 
+                                     State.Province != "alaska" & 
+                                     State.Province != "hawaii" &
+                                     Location == "Land",
+                                         adjName])
+  tempCountyMap <- map("county", region = temp_County_names,
+                      plot=FALSE, exact = TRUE, fill = TRUE)
   return(tempCountyMap)
 }
