45daae90ffa61e143ea9361bc83f98a0ac950ae1
colemand77 <dmacaricoleman@gmail.com>
2015-05-09 13:12:31 -0500

    Revise format to be full page map with overlayed controls and charts.

--- ui.R
+++ ui.R
@@ -5,58 +5,61 @@ source("graph_function.R")
 basinList <- unique(adj[,Basin])
 
 shinyUI(
-  fluidPage(
-    titlePanel("my Shiny App"),
-    sidebarLayout(
-      sidebarPanel(
-        sliderInput("dates", label = h3("Sliders"),
-                    min = min(rigCountDates), 
-                    max = max(rigCountDates), 
-                    value = max(rigCountDates),
-                    animate = TRUE),
+  navbarPage("RigExplorer", id ="nav",
+    tabPanel("Interactive Map",
+      div(class = "outer",
+          tags$head(
+            includeCSS("styles.css")),
+          
+        leafletOutput("myMap", width = "100%", height = "100%"),
         
-        checkboxGroupInput("UI_Checkbox", 
-                           label = "Filter By:",
-                           choices = list(
-                             "Basin Selector" = "Basin_select", 
-                             "Well Depth Selector" = "Depth_select",
-                             "Drilling For " = "DrillFor_select",
-                             "Direction Type" = "Trajectory_select",
-                             "Well Type" = "WellType_select"
-                           )),
-# to get it to work better on the shiny server        
-        radioButtons("details", "Show Breakdown:",
-                     c("All" = "True",
-                       "None" = "False"),
-                     selected = "False"),
-                
-        conditionalPanel(
-          condition = "input.details == 'True'",
-          uiOutput("basin"),
-          uiOutput("depth"),
-          uiOutput("drillfor"),
-          uiOutput("trajectory"),
-          uiOutput("welltype")),
-            
-        textOutput("DateUsed")
-        #textOutput("choseBasin"),
-       # textOutput("countyList")      
+        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                      draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                      width = 330, height = "auto",
+                      h2("Rig Explorer"),
+                      
+                      sliderInput("dates", label = h3("Sliders"),
+                                  min = min(rigCountDates), 
+                                  max = max(rigCountDates), 
+                                  value = max(rigCountDates),
+                                  animate = TRUE),
+                      textOutput("DateUsed"),
+                      
+                      radioButtons("details", "Show Breakdown:",
+                                   c("All" = "True",
+                                     "None" = "False"),
+                                   selected = "False"),
+                      
+                      conditionalPanel(
+                        condition = "input.details == 'True'",
+                        uiOutput("basin"),
+                        uiOutput("depth"),
+                        uiOutput("drillfor"),
+                        uiOutput("trajectory"),
+                        uiOutput("welltype"))
+                                            
+                      ),
         
-        ),
-      mainPanel(
-               leafletOutput("myMap"),
-               selectInput("graph_group", label = "group chart by",
-                           choices = list(
-                             "Basin" = "Basin",
-                             "Drill For" = "DrillFor",
-                             "Trajectory" = "Trajectory",
-                             "Well Type" = "WellType",
-                             "Well Depth" = "WellDepth",
-                             "none" = "none"),
-                           selected = "none"),
-               dygraphOutput("dygraph")
+        absolutePanel(id = "graph", class = "panel panel-default", fixed = FALSE,
+                      draggable = FALSE, top = "auto", left = 20, right = "auto", bottom = 20,
+                      width = 600, height = 400,
+                      h4("graph output"),
+                      selectInput("graph_group", label = "",
+                                  choices = list(
+                                    "Basin" = "Basin",
+                                    "Drill For" = "DrillFor",
+                                    "Trajectory" = "Trajectory",
+                                    "Well Type" = "WellType",
+                                    "Well Depth" = "WellDepth",
+                                    "none" = "none"),
+                                  selected = "none"),
+                      dygraphOutput("dygraph", width = "100%", height = "70%")
+                      )
       )
-      )
+    )
   )
 )
-
+        
+    
+    
+ 
